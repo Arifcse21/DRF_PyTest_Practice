9f4e2b9b3075bfaf6fc62da95a73768c6ca78c7c
Md Abdullah Al Arif <arif@archlinux.localdomain>
2023-02-12 10:56:47 +0600

    pytest fixture and parametrize

--- diodes/tests/test_models/test_diode_model.py
+++ diodes/tests/test_models/test_diode_model.py
@@ -1,9 +1,9 @@
-import pytest
-from diodes.models import Diode
+# import pytest
+# from diodes.models import Diode
 
-
-@pytest.mark.django_db
-def test_diode_model():
-    count = Diode.objects.count()
-
-    assert count == 0
+#
+# @pytest.mark.django_db
+# def test_diode_model():
+#     count = Diode.objects.count()
+#
+#     assert count == 0
